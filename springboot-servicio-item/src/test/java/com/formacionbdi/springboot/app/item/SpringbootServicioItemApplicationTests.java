7f15c5fbc93cb4adb9bf6a8c3a5d616123e415d6
Eduardo <eduardoartemis@gmail.com>
2024-04-09 16:38:29 -0500

    Video 17 -  09/04/2024 16:38

---
+++ springboot-servicio-item/src/test/java/com/formacionbdi/springboot/app/item/SpringbootServicioItemApplicationTests.java
@@ -0,0 +1,12 @@
+package com.formacionbdi.springboot.app.item;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioItemApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
